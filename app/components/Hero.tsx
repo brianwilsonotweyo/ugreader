e51574cb8627de8fc3e623caacdfbe059a89d61b
Libraula Benard <libraula@gmail.com>
2024-06-02 17:30:30 +0000

    jam jam

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -26,16 +26,16 @@ export default async function Hero() {
       <div className="mb-8 flex flex-wrap justify-between md:mb-16">
         <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
           <h1 className="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8 md:text-6xl">
-            Top Fashion for a top price!
+            Access thousands of books with our ereaders.
           </h1>
           <p className="max-w-md leading-relaxed text-gray-500 xl:text-lg">
-            We sell only the most exclusive and high quality products for you.
-            We are the best so come and shop with us.
+            Experience limitless reading pleasure -
+            thousands of books at your fingertips, wherever you go.
           </p>
         </div>
 
-        <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
-          <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
+        <div className="mb-12 flex w-full justify-center items-center md:mb-16 lg:w-2/3">
+          <div className="overflow-hidden rounded-lg bg-gray-100 shadow-lg">
             {data && data.image1 ? (
               <Image
                 src={urlFor(data.image1).url()}
@@ -49,18 +49,8 @@ export default async function Hero() {
               <div>There are no images</div>
             )}
           </div>
+</div>
 
-          <div className="overflow-hidden rounded-lg bg-gray-100 shadow-lg">
-            {/* <Image
-              src={urlFor(data.image2).url()}
-              alt="Great Photo"
-              className="h-full w-full object-cover object-center"
-              width={500}
-              height={500}
-              priority
-            /> */}
-          </div>
-        </div>
       </div>
 
       <div className="flex flex-col items-center justify-between gap-8 md:flex-row">
