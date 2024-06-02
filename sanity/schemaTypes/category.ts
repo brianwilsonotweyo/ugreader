1360629a5d224c77a026e5c84c2b52e48f250fee
Libraula Benard <libraula@gmail.com>
2024-06-02 08:15:36 +0000

    zenza

---
+++ sanity/schemaTypes/category.ts
@@ -0,0 +1,10 @@
+export default{
+    name: 'category',
+    type: 'document',
+    titlw: 'categories',
+    fields:[{
+        name: 'name',
+        title:'Name of Category',
+        type:'string',
+    }]
+}
