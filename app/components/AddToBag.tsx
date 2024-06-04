3e4cb655f13ce6e11f49cb3ac103283fe18fb178
brianwilsonotweyo <brianotweyo@gmail.com>
2024-06-04 15:59:51 +0000

    finals

--- app/components/AddToBag.tsx
+++ app/components/AddToBag.tsx
@@ -33,7 +33,7 @@ export default function AddToBag({
   };
 
   const handleWhatsAppMessage = () => {
-    const message = `*Product Name*: ${product.name}\n*Price*: ${product.price} ${product.currency}\n*Description*: ${product.description}\n*Product URL*: https://ugreader-5b2m.onrender.com/product/${product.slug}`;
+    const message = `*Product Name*: ${product.name}\n*Price*: ${product.price} ${product.currency}\n*Description*: ${product.description}\n*Product URL*: https://www.ugreader.com/product/${product.slug}`;
     const phoneNumber = "+256764725740";
     const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappURL, '_blank');
