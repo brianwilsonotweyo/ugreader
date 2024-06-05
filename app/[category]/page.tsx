516054e0a921f60f46a48d98f6436cc40abe5e6c
brianwilsonotweyo <44881487+brianwilsonotweyo@users.noreply.github.com>
2024-06-05 04:07:13 +0300

    UGX

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -66,7 +66,7 @@ export default async function CategoryPage({
                   </p>
                 </div>
                 <p className="text-sm font-medium text-gray-900">
-                  ${product.price}
+                  UGX {product.price}
                 </p>
               </div>
             </div>
